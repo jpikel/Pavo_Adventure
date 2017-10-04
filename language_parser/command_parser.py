48e2637fcc9713f04bab12272bd3d0a65e0329ea
Emily Caveness <emilycaveness@gmail.com>
2017-10-03 22:04:47 -0700

    Added a master_words file that contains information from the items, verbs, and rooms dicts in data and a script that can be used to generate the master_words file when the source dicts change. Minor fixes to the command_parser library and test_parser.py.

--- language_parser/command_parser.py
+++ language_parser/command_parser.py
@@ -1,53 +1,55 @@
 import re
 
-import noise_words as nw
+import noise_words as noise
 import prepositions as prep
+import master_words as words
 
 
 # Private functions
 # ----------------------------------------------------------------------------
-    def _preprocess(input_string):
-        """
-        Makes string lower case and removes any characters other than letters,
-        numbers, and a space.
-        """
-        preprocessed_string = input_string.lower()
-        # TODO: Add step to remove punctuation, etc.
-        return preprocessed_string
+def _preprocess(input_string):
+    """
+    Makes string lower case and removes any characters other than letters,
+    numbers, and a space.
+    """
+    preprocessed_string = input_string.lower()
+    # Remove any character that is not a number or letter.
+    preprocessed_string = re.sub('[^A-Za-z0-9 ]', '', preprocessed_string)
+    return preprocessed_string
 
-    def _check_for_exact_match(input_string):
-        """
-        TODO: Write docs
-        """
-        return 0
+def _check_for_exact_match(input_string):
+    """
+    TODO: Write docs
+    """
+    return 0
 
-    def _remove_noise(input_string):
-        """
-        TODO: Write docs
-        """
-        return 0
+def _remove_noise(input_string):
+    """
+    TODO: Write docs
+    """
+    return 0
 
-    def _normalize(input_string):
-        """
-        TODO: Write docs
-        """
-        return 0
+def _normalize(input_string):
+    """
+    TODO: Write docs
+    """
+    return 0
 
-    def _match_user_input_pattern(input_string):
-        """
-        TODO: Write docs
-        """
-        return 0
+def _match_user_input_pattern(input_string):
+    """
+    TODO: Write docs
+    """
+    return 0
 
     # TODO: Add additional methods to generate output dicts depending on match
 
 # Public function
 # ----------------------------------------------------------------------------
-    def parse_command(command):
-        """
-        TODO: Write docs
-        """
-        return 0
+def parse_command(command):
+    """
+    TODO: Write docs
+    """
+
 
 
 
@@ -62,3 +64,4 @@ import prepositions as prep
 # https://stackoverflow.com/questions/6797984/how-to-convert-string-to-lowercase-in-python
 # https://stackoverflow.com/questions/5843518/remove-all-special-characters-punctuation-and-spaces-from-string
 # https://docs.python.org/2/library/re.html
+
--- test_parser.py
+++ test_parser.py
@@ -2,5 +2,10 @@
 A program for testing methods in the command_parser module
 """
 
-import language_parser.command_parser as command_parser
+import language_parser.command_parser as parse
+
+test_input = "This is A TEST string!!!!"
+
+print "The test input is: " + test_input
+print "The test output is: " + parse._preprocess(test_input)
 
