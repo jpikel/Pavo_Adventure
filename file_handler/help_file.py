37bf599b2bbfcf945f590c9d1b835cbd4b4daef5
jpikel <pikelj@oregonstate.edu>
2017-10-13 08:16:10 +0000

    updated the help file

--- file_handler/help_file.py
+++ file_handler/help_file.py
@@ -7,17 +7,55 @@ Term - Fall 2017
 Description - 
 """
 
+import sys
+import curses
+import locale
 from name_lists import verb_info
 
-class help():
+def print_border():
+
+    print_stars_line(40)
+    sys.stdout.write("Help")
+    print_stars_line(40)
+    print_stars_two_cols(35)
+
+    print_stars_line(83)
+
+def print_stars_line(number):
+    """
+    prints x number of *
+    """
+    for _ in range(0, number):
+        sys.stdout.write("*")
+
+def print_stars_two_cols(num):
+    """
+    prints two columns of stars spaced 84 chars apart
+    """
+    for _ in range(0, num):
+        sys.stdout.write("*")
+        sys.stdout.write("*")
+    print "*".ljust(83)
+
+def move_cursor_x(num):
+    """
+    moves the cursor across the screen x cols
+    """
+
+
+def main():
     """
         a simple print to screen of the help file
     """
     verb_dict = verb_info().get_verb_definitions()
-    def display_help(self):
-        print("\n\n*******Help*******\n\n")
-        print("Verb       :: Action taken\n\n")
-        for key in self.verb_dict:
-            print(key.ljust(10) + " :: " + self.verb_dict[key])
+    print_border()
+    print("\nVerb       :: Action taken\n\n")
+    for key in verb_dict:
+        print(key.ljust(10) + " :: " +verb_dict[key])
 
-        print("\n\n")
+    print("\n\n")
+
+if __name__ == "__main__":
+    locale.setlocale(locale.LC_ALL, '')
+    code = locale.getpreferredencoding()
+    main()
