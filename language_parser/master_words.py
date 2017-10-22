fa0d8d7b099946c83cdd9c75a4e36b29b967d84b
Emily Caveness <emilycaveness@gmail.com>
2017-10-22 15:29:57 -0700

    Updated update_master_words.py to include features words

--- language_parser/master_words.py
+++ language_parser/master_words.py
@@ -1,11 +1,45 @@
+features = {
+   "footlocker": "footlocker", 
+   "heron": "heron", 
+   "clumps of bloody fur": "clumps of bloody fur", 
+   "sign": "sign", 
+   "stone marker": "stone marker", 
+   "shortwave radio": "shortwave radio", 
+   "rock shelf": "rock shelf", 
+   "dog sled": "dog sled", 
+   "wood pole": "wood pole", 
+   "deer carcass": "deer carcass", 
+   "log": "log", 
+   "overlook": "overlook", 
+   "bent pine": "bent pine", 
+   "blood stained snow": "blood stained snow", 
+   "snow capped island": "snow capped island", 
+   "small shelf": "small shelf", 
+   "canoe": "canoe", 
+   "hunting blind": "hunting blind", 
+   "leanto": "leanto", 
+   "locked safe": "locked safe", 
+   "plunge pool": "plunge pool", 
+   "animal corral": "animal corral", 
+   "campfire pit": "campfire pit", 
+   "camping cooler": "camping cooler", 
+   "small gorge": "small gorge", 
+   "driftwood": "driftwood", 
+   "island": "snow capped island", 
+   "storage shed": "storage shed", 
+   "bent tree": "bent tree", 
+   "tree stump": "tree stump", 
+   "wolves": "wolves"
+}
 items = {
    "frozen dead hare": "frozen dead hare", 
-   "old map": "old map", 
+   "light": "lantern", 
    "rescue whistle": "rescue whistle", 
-   "tattered notebook": "tattered notebook", 
+   "old map": "old map", 
    "can of sweetened condensed milk": "can of sweetened condensed milk", 
-   "can opener": "can opener", 
-   "paddle": "boat paddle", 
+   "candy bar": "candy bar", 
+   "lamp": "lantern", 
+   "tattered notebook": "spiral bound notepad", 
    "heavy winter parka": "heavy winter parka", 
    "flare gun": "flare gun", 
    "boat paddle": "boat paddle", 
@@ -21,6 +55,7 @@ rooms = {
    "game trail": "game trail", 
    "camp": "camp", 
    "crash site": "crash site", 
+   "trail": "game trail", 
    "field": "field", 
    "shore": "shore", 
    "waterfall": "waterfall", 
@@ -28,75 +63,78 @@ rooms = {
    "cavern": "cave", 
    "mountain base": "mountain base", 
    "river": "river", 
-   "mountain summit": "mountain summit"
+   "mountain summit": "mountain summit", 
+   "forest": "woods"
 }
 actions = {
    "move": "go", 
    "pull": "pull", 
    "search": "search", 
    "look at": "look at", 
-   "look": "look", 
+   "help": "help", 
    "read": "read", 
    "drop": "drop", 
-   "use": "use", 
+   "pick up": "take", 
+   "investigate": "search", 
    "inventory": "inventory", 
+   "use": "use", 
    "go": "go", 
    "take": "take", 
    "eat": "eat", 
-   "help": "help"
+   "look": "look"
 }
 all_words = {
-   "ranger station": {
-      "type": "room", 
-      "master_word": "ranger station"
+   "pick up": {
+      "type": "action", 
+      "master_word": "take"
    }, 
-   "cave": {
-      "type": "room", 
-      "master_word": "cave"
+   "footlocker": {
+      "type": "feature", 
+      "master_word": "footlocker"
+   }, 
+   "heron": {
+      "type": "feature", 
+      "master_word": "heron"
    }, 
    "help": {
       "type": "action", 
       "master_word": "help"
    }, 
-   "dense brush": {
-      "type": "room", 
-      "master_word": "dense brush"
-   }, 
-   "rapids": {
-      "type": "room", 
-      "master_word": "rapids"
-   }, 
-   "look at": {
-      "type": "action", 
-      "master_word": "look at"
-   }, 
-   "dusty old map": {
-      "type": "item", 
-      "master_word": "dusty old map"
-   }, 
-   "rescue whistle": {
-      "type": "item", 
-      "master_word": "rescue whistle"
+   "canoe": {
+      "type": "feature", 
+      "master_word": "canoe"
    }, 
    "search": {
       "type": "action", 
       "master_word": "search"
    }, 
+   "move": {
+      "type": "action", 
+      "master_word": "go"
+   }, 
    "can of sweetened condensed milk": {
       "type": "item", 
       "master_word": "can of sweetened condensed milk"
    }, 
-   "waterfall": {
-      "type": "room", 
-      "master_word": "waterfall"
+   "sign": {
+      "type": "feature", 
+      "master_word": "sign"
+   }, 
+   "inventory": {
+      "type": "action", 
+      "master_word": "inventory"
+   }, 
+   "old map": {
+      "type": "item", 
+      "master_word": "old map"
+   }, 
+   "stone marker": {
+      "type": "feature", 
+      "master_word": "stone marker"
    }, 
    "cavern": {
       "type": "room", 
       "master_word": "cave"
-   }, 
-   "heavy winter parka": {
-      "type": "item", 
-      "master_word": "heavy winter parka"
    }, 
    "go": {
       "type": "action", 
@@ -106,29 +144,173 @@ all_words = {
       "type": "action", 
       "master_word": "take"
    }, 
+   "investigate": {
+      "type": "action", 
+      "master_word": "search"
+   }, 
+   "shortwave radio": {
+      "type": "feature", 
+      "master_word": "shortwave radio"
+   }, 
+   "rock shelf": {
+      "type": "feature", 
+      "master_word": "rock shelf"
+   }, 
+   "dog sled": {
+      "type": "feature", 
+      "master_word": "dog sled"
+   }, 
+   "wood pole": {
+      "type": "feature", 
+      "master_word": "wood pole"
+   }, 
    "flare gun": {
       "type": "item", 
       "master_word": "flare gun"
    }, 
-   "eat": {
-      "type": "action", 
-      "master_word": "eat"
+   "deer carcass": {
+      "type": "feature", 
+      "master_word": "deer carcass"
+   }, 
+   "log": {
+      "type": "feature", 
+      "master_word": "log"
+   }, 
+   "overlook": {
+      "type": "feature", 
+      "master_word": "overlook"
+   }, 
+   "crash site": {
+      "type": "room", 
+      "master_word": "crash site"
+   }, 
+   "cave": {
+      "type": "room", 
+      "master_word": "cave"
    }, 
    "boat paddle": {
       "type": "item", 
       "master_word": "boat paddle"
    }, 
+   "field": {
+      "type": "room", 
+      "master_word": "field"
+   }, 
+   "lamp": {
+      "type": "item", 
+      "master_word": "lantern"
+   }, 
+   "bent pine": {
+      "type": "feature", 
+      "master_word": "bent pine"
+   }, 
+   "forest": {
+      "type": "room", 
+      "master_word": "woods"
+   }, 
+   "candy bar": {
+      "type": "item", 
+      "master_word": "candy bar"
+   }, 
+   "blood stained snow": {
+      "type": "feature", 
+      "master_word": "blood stained snow"
+   }, 
+   "rapids": {
+      "type": "room", 
+      "master_word": "rapids"
+   }, 
+   "snow capped island": {
+      "type": "feature", 
+      "master_word": "snow capped island"
+   }, 
+   "small gorge": {
+      "type": "feature", 
+      "master_word": "small gorge"
+   }, 
+   "mountain summit": {
+      "type": "room", 
+      "master_word": "mountain summit"
+   }, 
+   "woods": {
+      "type": "room", 
+      "master_word": "woods"
+   }, 
+   "ranger station": {
+      "type": "room", 
+      "master_word": "ranger station"
+   }, 
+   "look at": {
+      "type": "action", 
+      "master_word": "look at"
+   }, 
+   "hunting blind": {
+      "type": "feature", 
+      "master_word": "hunting blind"
+   }, 
+   "dense brush": {
+      "type": "room", 
+      "master_word": "dense brush"
+   }, 
+   "shore": {
+      "type": "room", 
+      "master_word": "shore"
+   }, 
+   "read": {
+      "type": "action", 
+      "master_word": "read"
+   }, 
+   "tattered notebook": {
+      "type": "item", 
+      "master_word": "spiral bound notepad"
+   }, 
+   "leanto": {
+      "type": "feature", 
+      "master_word": "leanto"
+   }, 
+   "locked safe": {
+      "type": "feature", 
+      "master_word": "locked safe"
+   }, 
+   "trail": {
+      "type": "room", 
+      "master_word": "game trail"
+   }, 
+   "plunge pool": {
+      "type": "feature", 
+      "master_word": "plunge pool"
+   }, 
+   "waterfall": {
+      "type": "room", 
+      "master_word": "waterfall"
+   }, 
+   "animal corral": {
+      "type": "feature", 
+      "master_word": "animal corral"
+   }, 
+   "heavy winter parka": {
+      "type": "item", 
+      "master_word": "heavy winter parka"
+   }, 
+   "campfire pit": {
+      "type": "feature", 
+      "master_word": "campfire pit"
+   }, 
+   "camping cooler": {
+      "type": "feature", 
+      "master_word": "camping cooler"
+   }, 
+   "eat": {
+      "type": "action", 
+      "master_word": "eat"
+   }, 
+   "small shelf": {
+      "type": "feature", 
+      "master_word": "small shelf"
+   }, 
    "pull": {
       "type": "action", 
       "master_word": "pull"
-   }, 
-   "move": {
-      "type": "action", 
-      "master_word": "go"
-   }, 
-   "game trail": {
-      "type": "room", 
-      "master_word": "game trail"
    }, 
    "frozen dead hare": {
       "type": "item", 
@@ -146,65 +328,65 @@ all_words = {
       "type": "room", 
       "master_word": "mountain ascent"
    }, 
-   "field": {
-      "type": "room", 
-      "master_word": "field"
+   "driftwood": {
+      "type": "feature", 
+      "master_word": "driftwood"
    }, 
-   "read": {
-      "type": "action", 
-      "master_word": "read"
-   }, 
-   "inventory": {
-      "type": "action", 
-      "master_word": "inventory"
+   "island": {
+      "type": "feature", 
+      "master_word": "snow capped island"
    }, 
    "camp": {
       "type": "room", 
       "master_word": "camp"
    }, 
-   "crash site": {
+   "game trail": {
       "type": "room", 
-      "master_word": "crash site"
+      "master_word": "game trail"
    }, 
-   "paddle": {
-      "type": "item", 
-      "master_word": "boat paddle"
+   "storage shed": {
+      "type": "feature", 
+      "master_word": "storage shed"
+   }, 
+   "clumps of bloody fur": {
+      "type": "feature", 
+      "master_word": "clumps of bloody fur"
    }, 
    "fire tower": {
       "type": "room", 
       "master_word": "fire tower"
    }, 
-   "can opener": {
-      "type": "item", 
-      "master_word": "can opener"
+   "bent tree": {
+      "type": "feature", 
+      "master_word": "bent tree"
    }, 
-   "shore": {
-      "type": "room", 
-      "master_word": "shore"
-   }, 
-   "woods": {
-      "type": "room", 
-      "master_word": "woods"
-   }, 
-   "mountain summit": {
-      "type": "room", 
-      "master_word": "mountain summit"
+   "tree stump": {
+      "type": "feature", 
+      "master_word": "tree stump"
    }, 
    "use": {
       "type": "action", 
       "master_word": "use"
    }, 
-   "tattered notebook": {
+   "light": {
       "type": "item", 
-      "master_word": "tattered notebook"
+      "master_word": "lantern"
    }, 
-   "mountain base": {
-      "type": "room", 
-      "master_word": "mountain base"
+   "rescue whistle": {
+      "type": "item", 
+      "master_word": "rescue whistle"
+   }, 
+   "wolves": {
+      "type": "feature", 
+      "master_word": "wolves"
    }, 
    "river": {
       "type": "room", 
       "master_word": "river"
+   }, 
+   "mountain base": {
+      "type": "room", 
+      "master_word": "mountain base"
    }, 
    "lantern": {
       "type": "item", 
